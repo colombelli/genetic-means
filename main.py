ce2b8a7da6e04152d48a4e0e37a3c6fe787fb1b7
colombelli <xcolombelli@gmail.com>
2019-11-22 05:10:52 -0300

    code updated for Windows compatibility

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import random
 import multiprocessing as mp
 from sklearn.cluster import KMeans
 
-df = pd.read_csv("leukemia_big.csv", header=None)
-x = df.T.iloc[0:, 1:]
-y = df.iloc[0:1].values[0]
 
-gm = GeneticMeans(x, y)
-gm.evolve()
+if __name__ == '__main__':  
+    df = pd.read_csv("leukemia_big.csv", header=None)
+    x = df.T.iloc[0:, 1:]
+    y = df.iloc[0:1].values[0]
+
+    gm = GeneticMeans(x, y)
+    gm.evolve()
